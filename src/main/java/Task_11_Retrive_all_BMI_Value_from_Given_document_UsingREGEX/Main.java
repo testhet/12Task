a981a6a2d958a61e04599b5985129947dc2b4a5c
hetGoti <testhetmail@gmail.com>
2025-04-23 18:40:03 +0530

    TAsk 11 Completed optimised

--- src/main/java/Task_11_Retrive_all_BMI_Value_from_Given_document_UsingREGEX/Main.java
+++ src/main/java/Task_11_Retrive_all_BMI_Value_from_Given_document_UsingREGEX/Main.java
@@ -11,9 +11,7 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) throws IOException {
        Path filePath = Paths.get("/home/hetgoti/Downloads/Problem_Input/11/TextDocument");
-        Pattern pattern1 = Pattern.compile("\\bBMI.*?(\\d+(?:\\.\\d)?)");
-        Pattern pattern2 = Pattern.compile("(?i)\\bB\\w*\\s+M\\w*\\s+I\\w*\\b.*?(\\d+(?:\\.\\d)?)");
-
+        Pattern pattern1 = Pattern.compile("(\\bBMI|(?i)\\bB\\w*\\s+M\\w*\\s+I\\w*\\b).*?(\\d+(\\.\\d*)?)");
 
        try(BufferedReader br = Files.newBufferedReader(filePath)){
             int count = 0;
@@ -21,13 +19,12 @@ public class Main {
            while ((lines = br.readLine()) != null){
                count++;
                Matcher matcher =  pattern1.matcher(lines);
-               Matcher matcher1 = pattern2.matcher(lines);
+
 
                if(matcher.find()){
-                   System.out.println("Line: "+count+" BMI: "+ matcher.group(1));
-               } else if (matcher1.find()) {
-                   System.out.println("Line: "+count+" BMI: "+ matcher1.group(1));
-               }  else {
+                   System.out.println("Line: "+count+" BMI: "+ matcher.group(2));
+               }
+               else {
                    System.out.println("no match found");
                }
            }
